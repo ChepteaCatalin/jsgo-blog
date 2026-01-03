e201bd2fe5eb895460f74ec91eaa101ceef9e957
Catalin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-01-03 18:44:24 +0200

    styling tweaks

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -4,6 +4,7 @@ import styles from './mdx-components.module.css';
 const components: MDXComponents = {
   h1: ({ children }) => <h1 className={styles.heading1}>{children}</h1>,
   p: ({ children }) => <p className={styles.paragraph}>{children}</p>,
+  pre: ({ children }) => <pre className={styles.pre}>{children}</pre>,
 };
 
 export function useMDXComponents(): MDXComponents {
