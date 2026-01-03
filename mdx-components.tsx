01efa912c0e0923ecb4d7f5da20337bfd2363bfc
Catalin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-01-03 19:57:00 +0200

    pure function article

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -4,6 +4,7 @@ import styles from './mdx-components.module.css';
 const components: MDXComponents = {
   h1: ({ children }) => <h1 className={styles.heading1}>{children}</h1>,
   p: ({ children }) => <p className={styles.paragraph}>{children}</p>,
+  li: ({ children }) => <li className={styles.listItem}>{children}</li>,
   pre: ({ children }) => <pre className={styles.pre}>{children}</pre>,
 };
 
