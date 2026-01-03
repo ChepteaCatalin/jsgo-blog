c3eb7a99e0807d4c4b4cee306bafccd715944824
Catalin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-01-03 16:38:18 +0200

    article heading title

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,6 +1,9 @@
 import type { MDXComponents } from 'mdx/types';
+import styles from './mdx-components.module.css';
 
-const components: MDXComponents = {};
+const components: MDXComponents = {
+  h1: ({ children }) => <h1 className={styles.h1}>{children}</h1>,
+};
 
 export function useMDXComponents(): MDXComponents {
   return components;
