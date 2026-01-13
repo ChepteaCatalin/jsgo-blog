eb7df7ea07ccb63efe1a805af6ac70f5b344b1b1
Catalin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-01-13 22:11:20 +0200

    placeholder blur for images

--- components/PostImage.tsx
+++ components/PostImage.tsx
@@ -5,7 +5,8 @@ export default function PostImage({
   src,
   title,
 }: Readonly<{
-  src: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  src: any;
   title: string;
 }>) {
   return (
@@ -14,9 +15,10 @@ export default function PostImage({
         src={src}
         alt={`Cover Image for ${title}`}
         fill
-        sizes="100vw"
+        sizes="(min-width: 1536px) 1476px, (min-width: 1280px) 1220px, (min-width: 1024px) 964px, (min-width: 768px) 708px, (min-width: 640px) 580px, calc(100vw - 60px)"
         loading="eager"
         fetchPriority="high"
+        placeholder="blur"
       />
     </div>
   );
