0227e9640edf6d709e97c9f9e0a691a6c1a3451c
Catalin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-01-20 19:34:20 +0200

    not found page metadata and rewordings'

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,17 @@
 import Link from 'next/link';
 import styles from './not-found.module.css';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Not Found',
+  description: 'The page you are looking for does not exist.',
+};
 
 export default function NotFound() {
   return (
     <div className={styles.container}>
       <h1>Not Found!</h1>
-      <p>Could not find the requested resource.</p>
+      <p>The page you are looking for does not exist.</p>
       <Link href="/" className={styles.link}>
         Go to the Homepage
       </Link>
