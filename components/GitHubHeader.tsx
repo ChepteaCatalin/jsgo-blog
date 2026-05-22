13cafe20350259cefaa10e536041b56dad5758c2
Catalin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-05-22 22:24:31 +0300

    target blank on github link + bump deps

--- components/GitHubHeader.tsx
+++ components/GitHubHeader.tsx
@@ -4,7 +4,11 @@ export default function GitHubHeader() {
   return (
     <div className={styles.container}>
       The source code for this blog is{' '}
-      <a href="https://github.com/ChepteaCatalin/jsgo-blog">
+      <a
+        href="https://github.com/ChepteaCatalin/jsgo-blog"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         available on GitHub
       </a>
     </div>
