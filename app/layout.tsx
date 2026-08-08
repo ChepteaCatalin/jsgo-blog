3def3d979ab979f444c41a45e91ee1b917810b73
Cătălin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-08-08 17:56:49 +0300

    add shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,16 @@
 import type { Metadata } from 'next';
-import { lora } from './fonts';
 import styles from './layout.module.css';
 import GitHubHeader from '@/components/GitHubHeader';
 import './globals.css';
 import './github.css';
+import { Lora, Geist_Mono } from 'next/font/google';
+import { cn } from '@/lib/utils';
+
+const lora = Lora({ subsets: ['latin'], variable: '--font-serif' });
+export const geistMono = Geist_Mono({
+  subsets: ['latin'],
+  variable: '--font-mono',
+});
 
 export const metadata: Metadata = {
   title: 'JavaScript: Global Offensive',
@@ -12,7 +19,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<'/'>) {
   return (
-    <html lang="en" className={lora.className}>
+    <html lang="en" className={cn(lora.className, 'font-serif', lora.variable)}>
       <body>
         <GitHubHeader />
         <div className={styles.container}>{children}</div>
