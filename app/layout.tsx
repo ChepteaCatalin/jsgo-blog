2d49b781b491aaebad80b2698bd34eea80a28dd0
Cătălin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-08-08 22:07:42 +0300

    github link

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,25 @@
-import type { Metadata } from 'next';
-import GitHubHeader from '@/components/GitHubHeader';
-import './globals.css';
-import './github.css';
-import { Lora, Geist_Mono } from 'next/font/google';
+import type { Metadata } from "next";
+import GitHubHeader from "@/components/GitHubHeader";
+import "./globals.css";
+import "./github.css";
+import { Lora, Geist_Mono } from "next/font/google";
 
 const lora = Lora({
-  subsets: ['latin'],
-  variable: '--font-lora',
+  subsets: ["latin"],
+  variable: "--font-lora",
 });
 
 const geistMono = Geist_Mono({
-  subsets: ['latin'],
-  variable: '--font-geist-mono',
+  subsets: ["latin"],
+  variable: "--font-geist-mono",
 });
 
 export const metadata: Metadata = {
-  title: 'JavaScript: Global Offensive',
+  title: "JavaScript: Global Offensive",
   description: `Opinionated takes on JavaScript and web development, challenging common misconceptions and emphasizing core concepts and a personal understanding of how things work.`,
 };
 
-export default function RootLayout({ children }: LayoutProps<'/'>) {
+export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en" className={`${lora.variable} ${geistMono.variable}`}>
       <body className="typeset typeset-docs max-w-[42em]">
--- components/GitHubHeader.tsx
+++ components/GitHubHeader.tsx
@@ -1,11 +1,12 @@
 export default function GitHubHeader() {
   return (
-    <div>
-      The source code for this blog is{' '}
+    <div className="bg-primary-foreground w-screen border-b px-5 py-1.25 text-center">
+      The source code for this blog is{" "}
       <a
         href="https://github.com/ChepteaCatalin/jsgo-blog"
         target="_blank"
         rel="noopener noreferrer"
+        className="transition-[color] duration-200 ease-in-out hover:text-[#0070f3]"
       >
         available on GitHub
       </a>
