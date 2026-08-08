4843f97991ed523da8f60d94ad619532289a7e74
Cătălin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-08-08 23:16:14 +0300

    post header

--- app/posts/layout.tsx
+++ app/posts/layout.tsx
@@ -4,7 +4,12 @@ import Link from "next/link";
 export default function PostsLayout({ children }: LayoutProps<"/posts">) {
   return (
     <main>
-      <Link href="/">JS:GO</Link>
+      <Link
+        href="/"
+        className="mt-8 inline-block text-4xl font-bold no-underline hover:underline"
+      >
+        JS:GO
+      </Link>
       <ModeToggle />
       <article>{children}</article>
     </main>
