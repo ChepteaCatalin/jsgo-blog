312dd1fd4b9e65a8320bd67587f07874ea14391c
Cătălin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-08-09 18:33:00 +0300

    reuse links

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,22 +5,26 @@ export default function Header() {
   return (
     <div className="bg-primary-foreground flex items-center justify-between border-b px-2 py-1">
       <div>
-        <a
-          href="https://github.com/ChepteaCatalin/jsgo-blog"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={buttonVariants({ variant: "ghost" })}
-        >
-          GitHub
-        </a>
-        <a
-          href="https://www.linkedin.com/in/catalin-cheptea"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={buttonVariants({ variant: "ghost" })}
-        >
-          LinkedIn
-        </a>
+        {[
+          {
+            href: "https://github.com/ChepteaCatalin/jsgo-blog",
+            label: "GitHub",
+          },
+          {
+            href: "https://www.linkedin.com/in/catalin-cheptea",
+            label: "LinkedIn",
+          },
+        ].map(({ href, label }) => (
+          <a
+            key={href}
+            href={href}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={buttonVariants({ variant: "ghost" })}
+          >
+            {label}
+          </a>
+        ))}
       </div>
       <ModeToggle />
     </div>
