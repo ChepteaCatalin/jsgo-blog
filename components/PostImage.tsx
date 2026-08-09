52ec5c7e87cbcb3d94929b2ccec0d869dac63251
Cătălin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2026-08-09 12:32:50 +0300

    improvements

--- components/PostImage.tsx
+++ components/PostImage.tsx
@@ -1,23 +1,20 @@
-import Image from 'next/image';
+import Image, { type StaticImageData } from "next/image";
 
 export default function PostImage({
   src,
   title,
 }: Readonly<{
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  src: any;
+  src: StaticImageData;
   title: string;
 }>) {
   return (
-    <div id={`img-wrapper-${title}`}>
-      <Image
-        src={src}
-        alt={`Cover Image for ${title}`}
-        sizes="100vw"
-        loading="eager"
-        fetchPriority="high"
-        placeholder="blur"
-      />
-    </div>
+    <Image
+      src={src}
+      alt={`Cover Image for ${title}`}
+      sizes="(max-width: 48rem) 100vw, 48rem"
+      style={{ width: "100%", height: "auto" }}
+      priority
+      placeholder="blur"
+    />
   );
 }
